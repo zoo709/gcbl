69c2e220779b991f51bd0fb94588a5dc3f1902d6
zoo709 <zooops709@gmail.com>
2024-08-07 22:18:35 +0900

    Create giftcodebookmarklet.js

---
+++ giftcodebookmarklet.js
@@ -0,0 +1,60 @@
+async function main_input_giftcode(sleep_interval_millsec = 1000) {
+  
+  var giftcode = prompt("Input GiftCode")
+  var members = prompt("Input Account IDs").split(/[,\s\r\n]/).filter(v => v != "")
+  
+  var result_summary = ""
+  
+  for (let i=0; i<members.length; ++i) {
+    var res_msg = await input_giftcode(members[i], giftcode)
+    
+    if (res_msg == "交換コードがありません") break
+    if (res_msg.startsWith("交換成功です。"))
+      result_summary += `\n${members[i]}\t${giftcode}\tOK`
+    else if (res_msg.startsWith("この報酬は受取済です。"))
+      result_summary += `\n${members[i]}\t${giftcode}\tAlready`
+    else
+      result_summary += `\n${members[i]}\t${giftcode}\tNG\t${res_msg}`
+
+    await sleep(sleep_interval_millsec)
+  }
+  
+  prompt("実行結果", result_summary)
+}
+
+async function input_giftcode(id, giftcode, sleep_interval_millsec) {
+  const SELECTOR_EXIT_BTN = "#app > div > div > div.exchange_container > div.main_content > div.roleInfo_con > div.exit_con > div"
+  const SELECTOR_CHARA_ID_INPUT = "#app > div > div > div.exchange_container > div.main_content > div.roleId_con > div.roleId_con_top > div.input_wrap > input[type=text]"
+  const SELECTOR_LOGIN_BTN = "#app > div > div > div.exchange_container > div.main_content > div.roleId_con > div.roleId_con_top > div.btn.login_btn > span"
+  const SELECTOR_GIFTCODE_INPUT = "#app > div > div > div.exchange_container > div.main_content > div.code_con > div.input_wrap > input[type=text]"
+  const SELECTOR_EXCHANGE_BTN = "#app > div > div > div.exchange_container > div.main_content > div.btn.exchange_btn"
+  const SELECTOR_POPUP_MESSAGE = "#app > div > div.message_modal > div.modal_content > p"
+  const SELECTOR_POPUP_CONFIRM_BTN = "#app > div > div.message_modal > div.modal_content > div.confirm_btn"
+
+
+  if (null != document.querySelector(SELECTOR_EXIT_BTN)) {
+    document.querySelector(SELECTOR_EXIT_BTN).click()
+  }
+
+  while (null == document.querySelector(SELECTOR_CHARA_ID_INPUT)) { await sleep(100) }
+  document.querySelector(SELECTOR_CHARA_ID_INPUT).value = id;
+  document.querySelector(SELECTOR_CHARA_ID_INPUT).dispatchEvent(new Event("input"));
+  document.querySelector(SELECTOR_LOGIN_BTN).click();
+
+  while (null == document.querySelector(SELECTOR_GIFTCODE_INPUT)) { await sleep(100) }
+  document.querySelector(SELECTOR_GIFTCODE_INPUT).value = giftcode;
+  document.querySelector(SELECTOR_GIFTCODE_INPUT).dispatchEvent(new Event("input"));
+  
+  await sleep(SLEEP_INTERVAL_MSEC)
+  document.querySelector(SELECTOR_EXCHANGE_BTN).click()
+
+  while (null == document.querySelector(SELECTOR_POPUP_CONFIRM_BTN)) { await sleep(100) }
+  var confirm_msg = document.querySelector(SELECTOR_POPUP_MESSAGE).innerText
+    
+  document.querySelector(SELECTOR_POPUP_CONFIRM_BTN).click()
+  return confirm_msg
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
